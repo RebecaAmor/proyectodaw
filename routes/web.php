fb5b31c4939a8aac610c58908fa6ee06b95a0171
Rebeca Amor <92539914+RebecaAmor@users.noreply.github.com>
2021-11-18 16:02:33 +0100

    Rutas admin

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,7 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+//RUTAS USUARIOS - Crear, almacenar, listar, mostrar, modificar, actualizar y eliminar.
 Route::get('/users/create', [App\Http\Controllers\UserController::class, 'create'])->name('users.create');
 
 Route::post('/users', [App\Http\Controllers\UserController::class, 'store'])->name('users.store');
@@ -40,3 +41,24 @@ Route::post('/users', [App\Http\Controllers\UserController::class, 'store'])->na
 Route::get('users', [App\Http\Controllers\UserController::class, 'index'])->name('users.index');
 
 Route::get('/users/{user}', [App\Http\Controllers\UserController::class, 'show'])->name('users.show');
+
+Route::get('/users/{user}/edit', [App\Http\Controllers\UserController::class, 'edit'])->name('users.edit');
+
+Route::put('/users/{user}', [App\Http\Controllers\UserController::class, 'update'])->name('users.update');
+
+Route::delete('/users/{user}', [App\Http\Controllers\UserController::class, 'destroy'])->name('users.delete');
+
+//RUTAS ADMIN - Crear, almacenar, listar, mostrar, modificar, actualizar y eliminar.
+Route::get('/admin/tcreate', [App\Http\Controllers\UserController::class, 'tcreate'])->name('admin.tcreate');
+
+Route::post('/admin', [App\Http\Controllers\UserController::class, 'tstore'])->name('admin.tstore');
+
+Route::get('trainners', [App\Http\Controllers\UserController::class, 'tindex'])->name('admin.tindex');
+
+Route::get('/admin/{trainner}', [App\Http\Controllers\UserController::class, 'tshow'])->name('admin.tshow');
+
+Route::get('/admin/{trainner}/tedit', [App\Http\Controllers\UserController::class, 'tedit'])->name('admin.tedit');
+
+Route::put('/admin/{trainner}', [App\Http\Controllers\UserController::class, 'tupdate'])->name('admin.tupdate');
+
+Route::delete('/admin/{trainner}', [App\Http\Controllers\UserController::class, 'tdestroy'])->name('admin.tdelete');
