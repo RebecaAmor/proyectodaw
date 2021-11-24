78b602f1b54e6d350eff6c0e180d4c7946092ee1
Rebeca Amor <92539914+RebecaAmor@users.noreply.github.com>
2021-11-24 01:31:16 +0100

    entrenadores y subs

---
+++ app/Models/Subscription.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+
+class Subscription extends Model
+{
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $table= 'subs';
+
+    protected $fillable = [
+        'subname', 'subtype', 'price',
+    ];
+
+    public $timestamps = false;
+}
