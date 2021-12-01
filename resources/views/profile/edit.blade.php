aa8e8119916927ef856baea912710de06306b96e
Rebeca Amor <92539914+RebecaAmor@users.noreply.github.com>
2021-12-01 09:12:04 +0100

    Add files via upload

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,11 +1,11 @@
-@extends('layouts.app', ['activePage' => 'profile', 'titlePage' => __('User Profile')])
+@extends('layouts.main', ['activePage' => 'profile', 'titlePage' => __('User Profile')])
 
 @section('content')
   <div class="content">
     <div class="container-fluid">
       <div class="row">
         <div class="col-md-12">
-          <form method="post" action="{{ route('profile.update') }}" autocomplete="off" class="form-horizontal">
+          <form method="post" action="{{ route('profile.userupdate', $user->id) }}" autocomplete="off" class="form-horizontal">
             @csrf
             @method('put')
 
