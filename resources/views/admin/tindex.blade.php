d8bd9cfd6c3a77485ba09d334c5b1105fceb49e7
Rebeca Amor <92539914+RebecaAmor@users.noreply.github.com>
2021-12-01 09:05:56 +0100

    actualizaciones

--- resources/views/admin/tindex.blade.php
+++ resources/views/admin/tindex.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.main', ['activePage'=>'trainners', 'titlePage'=>'Entrenadores'])
+@extends('layouts.main', ['activePage'=>'trainners', 'titlePage'=>'Monitores'])
 @section('content')
 <div class="content">
     <div class="container-fluid">
@@ -18,28 +18,37 @@
                             </div>
                             @endif
                                 <div class="row">
+                                @can('trainner_tcreate') 
                                     <div class="col-md-12 text-right">
                                         <a href="{{ route('admin.tcreate') }}" class="btn btn-sm btn-primary">Agregar entrenador</a>
                                     </div>
+                                @endcan
                                 </div>
                                 <div class="table-responsive">
                                     <table class="table">
                                         <thead class=" text-primary">
+                                            @can('trainner_tcreate')    
                                             <th>NIF</th>
+                                            @endcan
                                             <th>Nombre</th>
                                             <th>Email</th>
+                                            <th>Especialidad</th>
                                             <th class="text-right">Acciones</th>
                                         </thead>
                                         <tbody>
                                             @foreach ($trainners as $trainner)
                                                 <tr>
                                                     <!-- <td>{{ $trainner->id}}</td> -->
+                                                    @can('trainner_tcreate')
                                                     <td>{{ $trainner->tnif }}</td>
+                                                    @endcan
                                                     <td>{{ $trainner->tname }}</td>
                                                     <td>{{ $trainner->tmail }}</td>
+                                                    <td>{{ $trainner->sport }}</td>
                                                     <!-- <td>{{ $trainner->created_at}}</td> -->
                                                     <td class="td-actions text-right">
                                                         <a href="{{route('admin.tshow', $trainner->id)}}" class="btn btn-info"><i class="material-icons">info</i></a>
+                                                        @can('trainner_tcreate')
                                                         <a href="{{route('admin.tedit', $trainner->id)}}" class="btn btn-warning"><i class="material-icons">edit</i></a>
                                                         <form action="{{route('admin.tdelete', $trainner->id)}}" method="post" style="display:inline-block;" onsubmit="return confirm('Se van a eliminar todos los datos del monitor de forma definitiva')">
                                                         @csrf
@@ -48,6 +57,7 @@
                                                         <i class="material-icons">close</i>
                                                         </button>
                                                         </form>
+                                                        @endcan
                                                     </td>
                                                 </tr>
                                             @endforeach
--- resources/views/admin/tshow.blade.php
+++ resources/views/admin/tshow.blade.php
@@ -3,7 +3,7 @@
 <div class="content">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-12">
+            <div class="col-md-8">
                 <div class="card">
                     <div class="card-header card-header-primary">
                         <h4 class="card-title">{{ __('Entrenador') }}</h4>
@@ -11,32 +11,28 @@
                     </div>
                     <div class="card-body">
                         <div class="row">
-                            <div class="col-md-4">
-                                <div class="card card-user">
-                                    <div class="card-body">
-                                        <p class="card-text">
-                                            <div class="author">
-                                                <h5 class="title mt-3">{{$trainner->tname}}</h5>
-                                                <p class="description">
-                                                    {{$trainner->tnif}}<br>
-                                                    {{$trainner->tmail}}<br>
-                                                    {{$trainner->tbday}}<br>
-                                                    {{$trainner->sport}}<br>
-                                                </p>
-                                            </div>
-                                        </p>
-                                        <div class="card-description">
-                                            {{$trainner->cv}}
-                                        </div>
-                                    </div>
-                                    <div class="card-footer ml-auto mr-auto">
-                                        <div class="button-container">
-                                            <a href="{{route('admin.tedit', $trainner->id)}}" class="btn btn-sm btn-primary">Modificar</a>
-                                        </div>
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                    <div class="author">
+                                        <h5 class="title mt-3">{{$trainner->tname}}</h5>
+                                        @can('trainner_tedit')
+                                        <strong><p>DNI:</strong> {{$trainner->tnif}}</p>
+                                        <strong><p>Email:</strong> {{$trainner->tmail}}</p>
+                                        <strong><p>Fecha de nacimiento:</strong> {{$trainner->tbday}}</p>
+                                        @endcan
+                                        <strong><p>Especialidad:</strong> {{$trainner->sport}}</p>
+                                        <strong><p>CV:</strong> {{$trainner->cv}}</p>
                                     </div>
                                 </div>
+                                @can('trainner_tedit')
+                                <div class="card-footer ml-auto mr-auto">
+                                    <div class="button-container">
+                                        <a href="{{route('admin.tedit', $trainner->id)}}" class="btn btn-sm btn-primary">Modificar</a>
+                                    </div>
+                                </div>
+                                @endcan
                             </div>
-                        </div>    
+                        </div>
                     </div>
                 </div>
             </div>
